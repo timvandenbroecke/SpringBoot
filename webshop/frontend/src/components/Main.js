bb227242d339e7c19ce4e59b8abca4f9a950d987
timvandenbroecke <33755521+timvandenbroecke@users.noreply.github.com>
2023-04-09 21:13:36 +0200

    Add Slide animations for Main,js

--- webshop/frontend/src/components/Main.js
+++ webshop/frontend/src/components/Main.js
@@ -3,6 +3,7 @@ import Grid from '@mui/material/Grid';
 import Carousel from "./carousel/Carousel";
 import Paper from '@mui/material/Paper';
 import Divider from '@mui/material/Divider';
+import { Slide } from "react-awesome-reveal";
 
 class Main extends Component {
     constructor(){
@@ -18,22 +19,61 @@ class Main extends Component {
                         <Carousel />
                     </Grid>
                     <Grid item xs={12} md={12}>
-                        <div className="paper-watches">
-                            <Paper elevation={3}>
-                                <div>
-                                    <h3>Lorem Ipsum</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                    <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-                                    <br/>
-                                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
-                                </div>           
-                                <div className="image">
-                                    <img  src="https://images.unsplash.com/photo-1569397288884-4d43d6738fbd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80" />
-                                </div>               
-                            <Divider />
-
-                            </Paper>
-                        </div>
+                        <Slide direction="left" triggerOnce>
+                            <div className="paper-watches">
+                                
+                                    <Paper elevation={3}>
+                                        <div>
+                                            <h3>Lorem Ipsum</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                            <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                                            <br/>
+                                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+                                        </div>           
+                                        <div className="image">
+                                            <img  src="https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" />
+                                        </div>               
+                                    </Paper>
+                            </div>
+                        </Slide>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <Slide direction="right" triggerOnce>
+                            <div className="paper-jewels">
+                                
+                                    <Paper elevation={3}>
+                                    <div className="image">
+                                            <img  src="https://images.unsplash.com/photo-1611652022419-a9419f74343d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" />
+                                        </div>  
+                                        <div>
+                                            <h3>Lorem Ipsum</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                            <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                                            <br/>
+                                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+                                        </div>           
+                                    </Paper>
+                            </div>
+                        </Slide>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <Slide direction="left" triggerOnce>
+                            <div className="paper-watches">
+                                
+                                    <Paper elevation={3}>
+                                        <div>
+                                            <h3>Lorem Ipsum</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                            <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                                            <br/>
+                                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+                                        </div>           
+                                        <div className="image">
+                                            <img  src="https://images.unsplash.com/photo-1467043237213-65f2da53396f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
+                                        </div>               
+                                    </Paper>
+                            </div>
+                        </Slide>
                     </Grid>
 
                 </Grid>
