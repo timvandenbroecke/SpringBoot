1381a40b5148cc9618dee6481ff1693a9832a328
timvandenbroecke <33755521+timvandenbroecke@users.noreply.github.com>
2023-04-20 21:25:39 +0200

    Implementing sidebar selection index

--- hotel/frontend/src/components/SideBar.js
+++ hotel/frontend/src/components/SideBar.js
@@ -7,18 +7,28 @@ class SideBar extends Component {
         super();
 
         this.state = {
-
+            sidebar_active_1: true,
+            sidebar_active_2: false,
+            sidebar_active_3: false,
+            sidebar_active_4: false,
+            last_active: "sidebar_active_1"
         }
     }
 
 
+    handleSidebarActive = (name) => {
+        if(name !== this.state.last_active) this.setState({[name]: true, [this.state.last_active]: false, last_active: name});
+    }
+
 
     render(){
+
+        const {sidebar_active_1, sidebar_active_2, sidebar_active_3, sidebar_active_4} = this.state;
 
         return(
             <div className="sidebar">
                 <ul className="side-nav">
-                    <li className="side-nav__item">
+                    <li onClick={() => this.handleSidebarActive("sidebar_active_1")}  className={!sidebar_active_1 ? "side-nav__item" : "side-nav__item  side-nav__item--active"}>
                         <a href="#" className="side-nav__link">
                             <svg className="side-nav__icon">
                                 <use href={sprite + "#icon-home"} />
@@ -26,7 +36,7 @@ class SideBar extends Component {
                             <span>Hotel</span>
                         </a>
                     </li>
-                    <li className="side-nav__item">
+                    <li onClick={() => this.handleSidebarActive("sidebar_active_2")} className={!sidebar_active_2 ? "side-nav__item" : "side-nav__item  side-nav__item--active"}>
                         <a href="#" className="side-nav__link">
                             <svg className="side-nav__icon">
                                 <use href={sprite + "#icon-aircraft-take-off"} />
@@ -34,7 +44,7 @@ class SideBar extends Component {
                             <span>Flight</span>
                         </a>
                     </li>
-                    <li className="side-nav__item">
+                    <li onClick={() => this.handleSidebarActive("sidebar_active_3")}  className={!sidebar_active_3 ? "side-nav__item" : "side-nav__item  side-nav__item--active"}>
                         <a href="#" className="side-nav__link">
                             <svg className="side-nav__icon">
                                 <use href={sprite + "#icon-key"} />
@@ -42,7 +52,7 @@ class SideBar extends Component {
                             <span>Car rental</span>
                         </a>
                     </li>
-                    <li className="side-nav__item">
+                    <li onClick={() => this.handleSidebarActive("sidebar_active_4")}  className={!sidebar_active_4 ? "side-nav__item" : "side-nav__item  side-nav__item--active"}>
                         <a href="#" className="side-nav__link">
                             <svg className="side-nav__icon">
                                 <use href={sprite + "#icon-map"} />
